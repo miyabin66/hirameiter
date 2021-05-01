94b010878152e998a67da8f064d14420cf2ebec4
miyabin66 <freezemark5166@gmail.com>
2021-05-01 14:52:55 +0900

    なんとなく理解した

---
+++ components/top.tsx
@@ -0,0 +1,10 @@
+const top = () => {
+  return(
+    <div>
+      <h1>その時、ふと閃いたー</h1>
+      <button>始める</button>
+    </div>
+  )
+}
+
+export default top
---
+++ layouts/default.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default () => {
+  return(
+    <Head>
+      <title>hirameiter</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
