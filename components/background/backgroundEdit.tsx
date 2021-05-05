c8dd55f80d09e08205f8300fcb145d3033a30efe
miyabin66 <freezemark5166@gmail.com>
2021-05-05 17:30:39 +0900

    読み込めて入る

--- components/background/backgroundEdit.tsx
+++ components/background/backgroundEdit.tsx
@@ -1,13 +1,27 @@
+import { useRef } from 'react'
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
 import style from '~/styles/background/edit.module.scss'
 
 const select = (props) => {
+  const cropperRef = useRef(null)
+
   const setImage = () => {
+    const imageElement = cropperRef.current
+    const cropper = imageElement.cropper
+    props.setBackgroundScene(cropper.getCroppedCanvas().toDataURL())
     props.setScene('confirm')
     props.setBackgroundScene('select')
   }
+
   return(
     <div className={style.edit}>
-      <p>{props.selectedImage}</p>
+      <Cropper
+        src={props.selectedImage}
+        style={{ height: 400, width: "100%" }}
+        aspectRatio={9 / 16}
+        ref={cropperRef}
+      />
       <button onClick={setImage}>次へ</button>
     </div>
   )
--- components/confirm.tsx
+++ components/confirm.tsx
@@ -1,9 +1,60 @@
-import {useRef} from 'react'
-import style from '~/styles/background.module.scss'
+import {useEffect, useRef} from 'react'
+import style from '~/styles/confirm.module.scss'
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  PlaneGeometry,
+  TextureLoader,
+  MeshStandardMaterial,
+  Mesh,
+  DirectionalLight
+} from 'three'
 
 const confirm = (props) => {
+  useEffect(() => {
+    window.addEventListener('load', init)
+  })
+
+  const canvas = useRef(null)
+
+  const init = () => {
+    const width = 750
+    const height = 1334
+
+    const renderer = new WebGLRenderer({canvas})
+    renderer.setSize(width, height)
+
+    const scene = new Scene()
+
+    const camera = new PerspectiveCamera(45, width / height)
+    camera.position.set(0, 0, 10)
+
+    const geometry = new PlaneGeometry(1, 1)
+    const texture = new TextureLoader().load(props.background)
+    const material = new MeshStandardMaterial({
+      map: texture,
+    })
+    const mesh = new Mesh(geometry, material)
+    scene.add(mesh)
+
+    const directionalLight = new DirectionalLight(0xffffff)
+    directionalLight.position.set(1, 1, 1)
+    scene.add(directionalLight)
+
+    tick();
+
+    // 毎フレーム時に実行されるループイベントです
+    function tick() {
+      // レンダリング
+      renderer.render(scene, camera);
+
+      requestAnimationFrame(tick);
+    }
+  }
+
   return (
-    <p>confirm</p>
+    <canvas ref={canvas}></canvas>
   )
 }
 
