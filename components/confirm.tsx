ac905dd03426b69943d18dde4a23ab7f5ce1ecd7
miyabin66 <freezemark5166@gmail.com>
2021-05-05 18:49:40 +0900

    写りはするけど画像が荒い

--- components/confirm.tsx
+++ components/confirm.tsx
@@ -8,45 +8,52 @@ import {
   TextureLoader,
   MeshStandardMaterial,
   Mesh,
-  DirectionalLight
+  AmbientLight
 } from 'three'
 
 const confirm = (props) => {
-  const canvas = useRef(null)
-
-  const width = 750
-  const height = 1334
-
-  const renderer = new WebGLRenderer({canvas})
-  renderer.setSize(width, height)
-
-  const scene = new Scene()
-
-  const camera = new PerspectiveCamera(45, width / height)
-  camera.position.set(0, 0, 10)
-
-  const geometry = new PlaneGeometry(1, 1)
-  const texture = new TextureLoader().load(props.background)
-  const material = new MeshStandardMaterial({
-    map: texture,
+  useEffect(() => {
+    init()
   })
-  const mesh = new Mesh(geometry, material)
-  scene.add(mesh)
 
-  const directionalLight = new DirectionalLight(0xffffff)
-  directionalLight.position.set(1, 1, 1)
-  scene.add(directionalLight)
+  const init = () => {
+    const width = 750 / 2
+    const height = 1334 / 2
 
-  const tick = () => {
-    renderer.render(scene, camera);
+    const renderer = new WebGLRenderer({
+      canvas: document.querySelector('canvas')
+    })
 
-    requestAnimationFrame(tick);
+    const scene = new Scene()
+
+    const camera = new PerspectiveCamera(90, width / height)
+    camera.position.set(0, 0, height / 2)
+
+    const texture = new TextureLoader().load(props.background)
+    const geometry = new PlaneGeometry(width, height)
+    const material = new MeshStandardMaterial({
+      map: texture,
+    })
+    const mesh = new Mesh(geometry, material)
+    scene.add(mesh)
+
+    const ambientLight = new AmbientLight(0xffffff)
+    scene.add(ambientLight)
+
+    const tick = () => {
+      renderer.render(scene, camera);
+
+      requestAnimationFrame(tick);
+    }
+
+    tick()
   }
 
-  tick()
-
   return (
-    <canvas ref={canvas}></canvas>
+    <div>
+      <canvas id='canvas' className={style.confirm__canvas}></canvas>
+      <img src={props.background} className={style.confirm__canvas}></img>
+    </div>
   )
 }
 
