126c78d54e93a7c29d1436c08a3b1ec719ed8647
miyabin66 <freezemark5166@gmail.com>
2021-06-05 19:42:37 +0900

    整備

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,27 +1,10 @@
-import fs from 'fs'
 import Twitter from 'twitter'
 import dotenv from 'dotenv'
 import { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
 
 dotenv.config()
 
-interface twitter {
-  media_id: number
-  media_id_string: string
-  size: number
-  expires_after_secs: number
-  image: {
-    image_type: string
-    w: number
-    h: number
-  }
-}
-
-const upload = async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-): Promise<null> => {
+const upload = (req: NextApiRequest, res: NextApiResponse): void => {
   const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -30,15 +13,12 @@ const upload = async (
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   })
   const params = { media_data: req.body.image }
-  client.post('media/upload', params, (error, media, response) => {
+  client.post('media/upload', params, (error, media) => {
     if (error) {
-      console.log(response.errors)
       console.log(error)
     }
-    console.log(media)
     res.send({ id: media.media_id_string })
   })
-  return
 }
 
 export default upload
